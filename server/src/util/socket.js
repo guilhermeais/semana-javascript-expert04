8763223b17c60dc885a1442e9fb59fc6f9dc2a72
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-04-02 02:10:19 -0300

    feat: cria attach de eventos no SockertServer

--- server/src/util/socket.js
+++ server/src/util/socket.js
@@ -1,10 +1,29 @@
 import http from 'http'
 import { Server } from 'socket.io'
-
+import { constants } from './constants.js'
 export default class SocketServer {
   #io
-  constructor({ port }) {
+  constructor({ port } = {}) {
     this.port = port
+    this.namespaces = {}
+  }
+
+  attachEvents({ routeConfig }) {
+    for (const routes of routeConfig) {
+      for (const [namespace, { events, eventEmitter }] of Object.entries(
+        routes
+      )) {
+        const route = (this.namespaces[namespace] = this.#io.of(`/${namespace}`))
+
+        route.on('connection', socket => {
+          for (const [fnName, fn] of events) {
+            socket.on(fnName, (...args) => fn(socket, ...args))
+          }
+          
+          eventEmitter.emit(constants.event.USER_CONNECTED, socket)
+        })
+      }
+    }
   }
 
   async start() {
@@ -16,7 +35,7 @@ export default class SocketServer {
       res.end('I am connected!')
     })
 
-    this.io = new Server(server, {
+    this.#io = new Server(server, {
       cors: {
         origin: '*',
         credentials: false,
@@ -27,5 +46,14 @@ export default class SocketServer {
       server.on('error', reject)
       server.listen(this.port, () => resolve(server))
     })
+  }
+
+  async stop() {
+    if (!this.#io) {
+      return
+    }
+
+    await new Promise(resolve => this.#io.close(resolve))
+    this.#io = null
   }
 }
